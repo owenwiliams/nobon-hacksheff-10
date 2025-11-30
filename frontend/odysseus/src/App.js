bb8c838e7f6047355a635dc6fc0e49d45bed8a69
menbead <menbead0205@gmail.com>
2025-11-30 06:52:09 +0000

    Created necessary pages

--- frontend/odysseus/src/App.js
+++ frontend/odysseus/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import CustomCalendar from './components/CustomCalendar';
-
+import CustomSidebar from './components/CustomSidebar';
+import HomeAthenaChatbox from './components/HomeAthenaChatbox';
 
 function App() {
 
   return (
     <div className="App">
-        <h1>Odysseus Frontend</h1>
+        <h1>Odysseus</h1>
 
         <h2>Calendar</h2>
         <CustomCalendar />
+        <h2>button</h2>
+        <HomeAthenaChatbox />   
     </div>
   );
 }
---
+++ frontend/odysseus/src/pages/Journeys.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Journeys() {
+  return (
+    <div>
+      <h1>Journeys</h1>
+      <p>Journeys will be here</p>
+    </div>
+  );
+}
+
+export default Journeys;
