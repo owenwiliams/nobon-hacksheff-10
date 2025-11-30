782e1d88101297979150b2dcc05d5e4fa997ed95
Owen Williams <owen@extrabit.com>
2025-11-30 00:42:25 +0000

    fixed another merge error

--- backend/crud/quest_crud.py
+++ backend/crud/quest_crud.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+from datetime import date
 from sqlalchemy.orm import Session
 from models import Quest
 from schema import quest_schema as schema
@@ -16,16 +17,14 @@ def get_quest(db: Session, quest_id: int) -> Optional[Quest]:
 def get_all_quests(db: Session, skip: int = 0, limit: int = 100) -> List[Quest]:
     return db.query(Quest).offset(skip).limit(limit).all()
 
-<<<<<<< HEAD
 def get_quests_by_journey(db: Session, journey_id: int) -> List[Quest]:
     return db.query(Quest).filter(Quest.journey_id == journey_id).all()
-=======
+
 def get_quests_by_end_date(db: Session, end_date: date) -> List[Quest]:
     return db.query(Quest).filter(Quest.end_date == end_date).all()
 
 def get_active_quests(db: Session) -> List[Quest]:
     return db.query(Quest).filter(Quest.end_date.is_(None)).all()
->>>>>>> 67ed5e1 (changes to journey and quest crud for end data quiries)
 
 def update_quest(db: Session, quest_id: int, quest_in: QuestUpdate) -> Optional[Quest]:
     db_quest = db.query(Quest).filter(Quest.id == quest_id).first()
